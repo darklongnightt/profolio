f067b8231548370fe0cbdc475cde570c8663243b
Xavier Xu <ftt.xavier.xu@grabtaxi.com>
2020-05-09 18:41:17 +0800

    UI-improvements to publishing and analytics

--- portfolio/src/components/dashboard/Analytics.js
+++ portfolio/src/components/dashboard/Analytics.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
@@ -6,36 +6,50 @@ import moment from "moment";
 import TodayViews from "./TodayViews";
 import Visualization from "./Visualization";
 
-const Analytics = props => {
-  const { views } = props;
-
-  if (views && views.length) {
-    const todayViews = getTodayViews(views);
-    return (
-      <div className="card z-depth-0 modal-container">
-        <div className="card-content center">
-          <h5 className="bold">Profile Analytics</h5>
-          <Visualization viewsData={views} />
-          <div className="divider"></div>
-          <TodayViews views={todayViews} />
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="card z-depth-0 modal-container">
-        <div className="card-content center">
-          <TodayViews views={0} />
-        </div>
-      </div>
-    );
+class Analytics extends Component {
+  state = {
+    todayViews: 0,
   }
-};
 
-const getTodayViews = views => {
-  const today = moment().format("YYYY-MM-DD");
-  const todayViews = views.find(elems => elems.id === today);
-  return todayViews === undefined ? 0 : todayViews.count;
+  getTodayViews = (views) => {
+    const today = moment().format("YYYY-MM-DD");
+    const todayViews = views.find(elems => elems.id === today);
+    console.log(todayViews)
+    this.setState({
+      todayViews: todayViews === undefined ? 0 : todayViews.count,
+    });
+  };
+
+  componentWillReceiveProps(nextProps) {
+    const { views } = nextProps;
+    views && this.getTodayViews(views);
+  }
+
+  render() {
+    const { views } = this.props;
+    const { todayViews } = this.state;
+
+    if (views && views.length) {
+      return (
+        <div className="card z-depth-0 modal-container">
+          <div className="card-content center">
+            <h5 className="bold">Profile Analytics</h5>
+            <Visualization viewsData={views} />
+            <div className="divider"></div>
+            <TodayViews views={todayViews} />
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="card z-depth-0 modal-container">
+          <div className="card-content center">
+            <TodayViews views={0} />
+          </div>
+        </div>
+      );
+    }
+  }
 };
 
 const mapStateToProps = state => {
@@ -53,7 +67,6 @@ export default compose(
         collection: "users",
         doc: props.auth.uid,
         subcollections: [{ collection: "views" }],
-        orderBy: ["date"],
         storeAs: "views"
       }
     ];
